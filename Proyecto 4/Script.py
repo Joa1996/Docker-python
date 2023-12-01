2842194e4a04597654d19093f64a43ec9689a892
Joa1996 <joaquingallardo096@gmail.com>
2023-11-30 22:57:53 -0300

    Ultm. Ajuste

--- Proyecto 4/Script.py
+++ Proyecto 4/Script.py
@@ -1,7 +1,9 @@
 import pandas as pd 
-
+import os #importamos la libreria "os" para usar las variables de entorno, en nuestro caso
+#es la variabl de entorno de file_value
 # Obtenemos la ruta del archivo desde la variable de entorno
-ruta ='./data/File.csv'
+value=os.environ.get('file_value') #Obtenemos la variable de entorno
+ruta =f'./data/{value}'
 
 df=pd.read_csv(ruta,header=0)
 df.loc['Total',:] = df.sum(axis=0)# Sumamos Total
